a4a2b6158bd8eec36e89b3b333eca25e0a29fde6
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-09 08:49:25 +0430

    Load image and initialize MTCNN face detector.

--- faceEmotion.py
+++ faceEmotion.py
@@ -3,3 +3,9 @@ from mtcnn import MTCNN
 import numpy as np
 from keras.models import load_model
 from random import choice
+
+image_path = r"image1.jpg"
+image = cv2.imread(image_path)
+
+detector = MTCNN()
+
