d360ba51b1f19833ccba4f2bc450c6f0f26ea95b
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-09 08:59:50 +0430

    emotion_labels & colors added.

--- faceEmotion.py
+++ faceEmotion.py
@@ -11,3 +11,23 @@ detector = MTCNN()
 
 faces = detector.detect_faces(image)
 emotion_model = load_model(r"model_v6_23.hdf5" )
+
+emotion_labels = {
+    "Angry": 0,
+    "Sad": 5,
+    "Neutral": 4,
+    "Disgust": 1,
+    "Surprise": 6,
+    "Fear": 2,
+    "Happy": 3,
+}
+
+colors = [
+    (255, 0, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+    (255, 0, 255),
+    (255, 69, 0),
+    (0, 128, 0),
+    (139, 0, 139),
+]
